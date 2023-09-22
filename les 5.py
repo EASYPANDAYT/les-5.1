04a6cde2e3bc7b599cc1174417b4192aebf6027b
EASYPANDAYT <145465554+EASYPANDAYT@users.noreply.github.com>
2023-09-22 17:44:09 +0300

    Add files via upload

---
+++ les 5.py
@@ -0,0 +1,47 @@
+#age  = int (input ('Сколько тебе лет? '))
+
+
+#print (age, 'год')
+#print (age, 'года')
+
+
+#if age % 10 == 0 :
+    #print (age, 'лет')
+#elif age % 10 == 1 and age != 11:
+   # print (age, 'год')
+#elif age >10 and age < 15:
+  #  print (age, 'лет')
+#elif age % 10 > 4 :
+    #print (age, 'лет')
+#else :
+    #print (age, 'года')
+
+
+#chislo = int (input ('Напиши число, а я скажу четное оно или нет <3 '))
+#if chislo % 2 == 0 :
+#    print (chislo, 'четное')
+#else :
+#    print (chislo, 'нечетное')
+
+
+
+t = int (input ('Какая температура на  улице? '))
+if t >20 and t <30 :
+    print ('надень панаму')
+if t >10 and t <21 :
+    print ('надень кепку')
+if t  >0  and t < 11 :
+    print ('надень ветровку')
+if t > -10 and t < 0 :
+    print ('надень свитер')
+if t > -20 and t < -10 :
+    print (' надень шапку')
+elif t <-20 :
+    print ('лучше остаться дома')
+elif t >30 :
+    print ('лучше остаться дома')
+
+
+
+
+
